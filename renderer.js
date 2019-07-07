4b5589103bdc08076b2cf6b25d2ebd8a049f68db
Eric Link <elink@examsoft.com>
2019-07-07 10:41:32 -0500

    merged

--- renderer.js
+++ renderer.js
@@ -1,14 +1,2 @@
 // remember window location
 require('electron-cookies')
-
-// open all links in external browser
-document.addEventListener('click', (event) => {
-  console.log(event);
-  if (event.target.tagName === 'A' && event.target.href.startsWith('http')) {
-    event.preventDefault();
-    require('electron').shell.openExternal(event.target.href);
-  }
-})
-
-
-
